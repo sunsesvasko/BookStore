117b1713a20809ebbb67af4e60293ee995fe8e31
Vasko <vbblajev@gmail.com>
2024-04-05 13:47:53 +0300

    first commit

---
+++ backend/app.js
@@ -0,0 +1,6 @@
+// MODULES
+const express = require("express");
+
+const app = express();
+
+module.exports = app;
---
+++ backend/server.js
@@ -0,0 +1,11 @@
+// Modules
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+
+const app = require("./app");
+
+const port = process.env.PORT;
+
+const server = app.listen(port, () =>
+  console.log(`App running on port: ${port}`)
+);
