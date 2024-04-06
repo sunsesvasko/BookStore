ad10bb753c109be0d87b032101466658772df4b6
Vasko <vbblajev@gmail.com>
2024-04-06 19:14:11 +0300

    Comments

--- backend/app.js
+++ backend/app.js
@@ -14,16 +14,21 @@ app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: true, limit: "10kb" }));
 
 // === ROUTES === //
+
+// View Routes
 app.get("/", (req, res) => {
   return res.status(200).send("Welcome to MERN Stack Tutorial");
 });
 
+// Book Routes
 app.use("/api/books", bookRoutes);
 
+// Other Routes
 app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`), 404);
 });
 
+// Global Error Handler
 app.use(globalErrorHandler);
 
 module.exports = app;
