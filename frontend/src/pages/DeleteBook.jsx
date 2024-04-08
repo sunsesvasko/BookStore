e8f45a73b9712662f96523dbc994318d84e4159a
Vasko <vbblajev@gmail.com>
2024-04-08 14:51:45 +0300

    All CRUD operations can now be performed on the frontend

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -1,5 +1,45 @@
+import { useState } from "react";
+import BackButton from "./../components/BackButton";
+import Spinner from "./../components/Spinner";
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
+
 const DeleteBook = () => {
-  return <div></div>;
+  const [loading, setLoading] = useState(false);
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const handleDeleteBook = () => {
+    setLoading(true);
+    axios
+      .delete(`http://localhost:3000/api/books/${id}`)
+      .then(() => {
+        setLoading(false);
+        alert("Book deleted successfully!");
+        navigate("/");
+      })
+      .catch((err) => {
+        setLoading(false);
+        alert("Error deleting book!");
+        console.log(err);
+      });
+  };
+  return (
+    <div className="p-4">
+      <BackButton />
+      <h1 className="my-4 text-3xl">Delete Book</h1>
+      {loading && <Spinner />}
+      <div className="flex flex-col items-center border-2 border-sky-400 rounded-xl w-[600px] p-8 mx-auto">
+        <h3 className="text-3xl">Are you sure you want to delete this book?</h3>
+        <button
+          className="w-full p-4 m-8 text-white bg-red-600"
+          onClick={handleDeleteBook}
+        >
+          Yes, Delete It!
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default DeleteBook;
