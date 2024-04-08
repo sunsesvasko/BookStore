dcab61334d87b9c9c593b046f38689faa16fd9ff
Vasko <vbblajev@gmail.com>
2024-04-08 15:11:04 +0300

    Display books as a table or as cards

---
+++ frontend/src/components/home/BooksCard.jsx
@@ -0,0 +1,45 @@
+import { Link } from "react-router-dom";
+import { PiBookOpenTextLight } from "react-icons/pi";
+import { BiUserCircle } from "react-icons/bi";
+import { AiOutlineEdit } from "react-icons/ai";
+import { BsInfoCircle } from "react-icons/bs";
+import { MdOutlineDelete } from "react-icons/md";
+
+const BooksCard = ({ books }) => {
+  return (
+    <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      {books.map((book) => (
+        <div
+          key={book._id}
+          className="relative px-4 py-2 m-4 border-2 border-gray-500 rounded-lg hover:shadow-xl"
+        >
+          <h2 className="absolute px-4 py-1 bg-red-300 rounded-lg top-1 right-2">
+            {book.publishYear}
+          </h2>
+          <h4 className="my-2 text-gray-500">{book._id}</h4>
+          <div className="flex items-center justify-start gap-x-4">
+            <PiBookOpenTextLight className="text-2xl text-red-300" />
+            <h2 className="my-1">{book.title}</h2>
+          </div>
+          <div className="flex items-center justify-start gap-x-4">
+            <BiUserCircle className="text-2xl text-red-300" />
+            <h2 className="my-1">{book.author}</h2>
+          </div>
+          <div className="flex items-center justify-between p-4 mt-4 gap-x-2">
+            <Link to={`/books/details/${book._id}`}>
+              <BsInfoCircle className="text-2xl text-green-800 hover:text-black" />
+            </Link>
+            <Link to={`/books/edit/${book._id}`}>
+              <AiOutlineEdit className="text-2xl text-yellow-600 hover:text-black" />
+            </Link>
+            <Link to={`/books/delete/${book._id}`}>
+              <MdOutlineDelete className="text-2xl text-red-600 hover:text-black" />
+            </Link>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default BooksCard;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,13 +2,14 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import Spinner from "../components/Spinner";
 import { Link } from "react-router-dom";
-import { AiOutlineEdit } from "react-icons/ai";
-import { BsInfoCircle } from "react-icons/bs";
-import { MdOutlineAddBox, MdOutlineDelete } from "react-icons/md";
+import { MdOutlineAddBox } from "react-icons/md";
+import BooksCard from "../components/home/BooksCard";
+import BooksTable from "../components/home/BooksTable";
 
 const Home = () => {
   const [books, setBooks] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [showType, setShowType] = useState("table");
 
   useEffect(() => {
     setLoading(true);
@@ -25,62 +26,31 @@ const Home = () => {
   }, []);
   return (
     <div className="p-4">
-      <div className="flex items-center justify-between">
+      <div className="flex items-center justify-center gap-x-4">
+        <button
+          className="px-4 py-1 rounded-lg bg-sky-300 hover:bg-sky-600"
+          onClick={() => setShowType("table")}
+        >
+          Table
+        </button>
+        <button
+          className="px-4 py-1 rounded-lg bg-sky-300 hover:bg-sky-600"
+          onClick={() => setShowType("card")}
+        >
+          Card
+        </button>
+      </div>
+      <div className="flex flex-col items-center justify-between">
         <h1 className="my-8 text-3xl ">Books List</h1>
         <Link to="/books/create">
           <MdOutlineAddBox className="text-4xl text-sky-800" />
         </Link>
         {loading ? (
           <Spinner />
+        ) : showType === "table" ? (
+          <BooksTable books={books} />
         ) : (
-          <table className="w-full border-separate border-spacing-2">
-            <thead>
-              <tr>
-                <th className="border rounded-md border-slate-600">No</th>
-                <th className="border rounded-md border-slate-600">Title</th>
-                <th className="border rounded-md border-slate-600 max-md:hidden">
-                  Author
-                </th>
-                <th className="border rounded-md border-slate-600 max-md:hidden">
-                  Publish Year
-                </th>
-                <th className="border rounded-md border-slate-600">
-                  Operations
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {books.map((book, index) => (
-                <tr key={book._id} className="h-8">
-                  <td className="text-center border rounded-md border-slate-700">
-                    {index + 1}
-                  </td>
-                  <td className="text-center border rounded-md border-slate-700">
-                    {book.title}
-                  </td>
-                  <td className="text-center border rounded-md border-slate-700 max-md:hidden">
-                    {book.author}
-                  </td>
-                  <td className="text-center border rounded-md border-slate-700 max-md:hidden">
-                    {book.publishYear}
-                  </td>
-                  <td className="text-center border rounded-md border-slate-700 max-md:hidden">
-                    <div className="flex justify-center gap-x-4 ">
-                      <Link to={`/books/details/${book._id}`}>
-                        <BsInfoCircle className="text-2xl text-green-800" />
-                      </Link>
-                      <Link to={`/books/edit/${book._id}`}>
-                        <AiOutlineEdit className="text-2xl text-yellow-600" />
-                      </Link>
-                      <Link to={`/books/delete/${book._id}`}>
-                        <MdOutlineDelete className="text-2xl text-red-600" />
-                      </Link>
-                    </div>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+          <BooksCard books={books} />
         )}
       </div>
     </div>
